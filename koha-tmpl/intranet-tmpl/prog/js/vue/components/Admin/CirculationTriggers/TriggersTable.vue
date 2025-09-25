2b1be476ba8422606add03dc9c3fea0e5a9dc0c6
Chloe Zermatten <chloe.zermatten@openfifth.co.uk>
2025-09-25 16:07:52 +0000

    FIXUP: display explicitly set rules
    ..because I forgot that contrary to exhaustive effective, effextice sets would be of varying lengths for different triggers, which the template was not handling. SQUASH BEFORE SUBMITTING

--- koha-tmpl/intranet-tmpl/prog/js/vue/components/Admin/CirculationTriggers/TriggersTable.vue
+++ koha-tmpl/intranet-tmpl/prog/js/vue/components/Admin/CirculationTriggers/TriggersTable.vue
@@ -37,275 +37,218 @@
             </th>
         </thead>
         <tbody>
-            <tr
+            <template
                 v-for="(ruleSet, i) in ruleSets"
-                v-if="ruleSets"
                 :key="'ruleSet' + i"
                 :class="{
                     selected_rule_set:
                         modal && i + 1 === parseInt(activeTriggerBeingEdited),
                 }"
             >
-                <td
-                    v-if="
-                        !modal &&
-                        ruleSet[
-                            `overdue_${modal ? i + 1 : triggerNumber}_has_rules`
-                        ]
-                    "
-                    class="trigger_context"
-                >
-                    {{
-                        handleContext(
-                            ruleSet.context.library_id,
-                            libraries,
-                            "library_id"
-                        )
-                    }}
-                </td>
-                <td
-                    v-if="
-                        !modal &&
-                        ruleSet[
-                            `overdue_${modal ? i + 1 : triggerNumber}_has_rules`
-                        ]
-                    "
-                    class="trigger_context"
-                >
-                    {{
-                        handleContext(
-                            ruleSet.context.patron_category_id,
-                            patronCategories,
-                            "patron_category_id"
-                        )
-                    }}
-                </td>
-                <td
-                    v-if="
-                        !modal &&
-                        ruleSet[
-                            `overdue_${modal ? i + 1 : triggerNumber}_has_rules`
-                        ]
-                    "
-                    class="trigger_context border_right"
-                >
-                    {{
-                        handleContext(
-                            ruleSet.context.item_type_id,
-                            itemTypes,
-                            "item_type_id",
-                            "description"
-                        )
-                    }}
-                </td>
-                <td
-                    v-if="
-                        modal &&
-                        ruleSet[
-                            `overdue_${modal ? i + 1 : triggerNumber}_has_rules`
-                        ]
-                    "
-                >
-                    {{ i + 1 }}
-                </td>
-                <!-- Delay -->
-                <td
+                <tr
                     v-if="
                         ruleSet[
                             `overdue_${modal ? i + 1 : triggerNumber}_has_rules`
                         ]
                     "
                 >
-                    <span
-                        :class="{
-                            fallback:
+                    <td v-if="!modal" class="trigger_context">
+                        {{
+                            handleContext(
+                                ruleSet.context.library_id,
+                                libraries,
+                                "library_id"
+                            )
+                        }}
+                    </td>
+                    <td v-if="!modal" class="trigger_context">
+                        {{
+                            handleContext(
+                                ruleSet.context.patron_category_id,
+                                patronCategories,
+                                "patron_category_id"
+                            )
+                        }}
+                    </td>
+                    <td v-if="!modal" class="trigger_context border_right">
+                        {{
+                            handleContext(
+                                ruleSet.context.item_type_id,
+                                itemTypes,
+                                "item_type_id",
+                                "description"
+                            )
+                        }}
+                    </td>
+                    <td v-if="modal">
+                        {{ i + 1 }}
+                    </td>
+                    <!-- Delay -->
+                    <td>
+                        <span
+                            :class="{
+                                fallback:
+                                    ruleSet[
+                                        `overdue_${modal ? i + 1 : triggerNumber}_delay`
+                                    ].isFallback,
+                            }"
+                        >
+                            {{
                                 ruleSet[
                                     `overdue_${modal ? i + 1 : triggerNumber}_delay`
-                                ].isFallback,
-                        }"
-                    >
-                        {{
-                            ruleSet[
-                                `overdue_${modal ? i + 1 : triggerNumber}_delay`
-                            ].value
-                        }}
-                    </span>
-                </td>
-                <!--  Notice -->
-                <td
-                    v-if="
-                        ruleSet[
-                            `overdue_${modal ? i + 1 : triggerNumber}_has_rules`
-                        ]
-                    "
-                >
-                    <span
-                        :class="{
-                            fallback:
-                                ruleSet[
-                                    `overdue_${modal ? i + 1 : triggerNumber}_notice`
-                                ].isFallback,
-                        }"
-                    >
-                        {{
-                            handleNotice(
-                                ruleSet[
-                                    `overdue_${modal ? i + 1 : triggerNumber}_notice`
                                 ].value
-                            )
-                        }}
-                    </span>
-                </td>
-                <!-- Email -->
-                <td
-                    v-if="
-                        ruleSet[
-                            `overdue_${modal ? i + 1 : triggerNumber}_has_rules`
-                        ]
-                    "
-                >
-                    <span
-                        :class="{
-                            fallback:
+                            }}
+                        </span>
+                    </td>
+                    <!--  Notice -->
+                    <td>
+                        <span
+                            :class="{
+                                fallback:
+                                    ruleSet[
+                                        `overdue_${modal ? i + 1 : triggerNumber}_notice`
+                                    ].isFallback,
+                            }"
+                        >
+                            {{
+                                handleNotice(
+                                    ruleSet[
+                                        `overdue_${modal ? i + 1 : triggerNumber}_notice`
+                                    ].value
+                                )
+                            }}
+                        </span>
+                    </td>
+                    <!-- Email -->
+                    <td>
+                        <span
+                            :class="{
+                                fallback:
+                                    ruleSet[
+                                        `overdue_${modal ? i + 1 : triggerNumber}_mtt`
+                                    ].isFallback,
+                            }"
+                        >
+                            {{
+                                handleTransport(
+                                    ruleSet[
+                                        `overdue_${modal ? i + 1 : triggerNumber}_mtt`
+                                    ].value,
+                                    "email",
+                                    !ruleSet[
+                                        `overdue_${modal ? i + 1 : triggerNumber}_notice`
+                                    ].value
+                                )
+                            }}
+                        </span>
+                    </td>
+                    <!-- Print -->
+                    <td>
+                        <span
+                            :class="{
+                                fallback:
+                                    ruleSet[
+                                        `overdue_${modal ? i + 1 : triggerNumber}_mtt`
+                                    ].isFallback,
+                            }"
+                        >
+                            {{
+                                handleTransport(
+                                    ruleSet[
+                                        `overdue_${modal ? i + 1 : triggerNumber}_mtt`
+                                    ].value,
+                                    "print",
+                                    !ruleSet[
+                                        `overdue_${modal ? i + 1 : triggerNumber}_notice`
+                                    ].value
+                                )
+                            }}
+                        </span>
+                    </td>
+                    <!-- SMS -->
+                    <td>
+                        <span
+                            :class="{
+                                fallback:
+                                    ruleSet[
+                                        `overdue_${modal ? i + 1 : triggerNumber}_mtt`
+                                    ].isFallback,
+                            }"
+                        >
+                            {{
+                                handleTransport(
+                                    ruleSet[
+                                        `overdue_${modal ? i + 1 : triggerNumber}_mtt`
+                                    ].value,
+                                    "sms",
+                                    !ruleSet[
+                                        `overdue_${modal ? i + 1 : triggerNumber}_notice`
+                                    ].value
+                                )
+                            }}
+                        </span>
+                    </td>
+                    <!-- Restricts Checkouts -->
+                    <td>
+                        <span
+                            :class="{
+                                fallback:
+                                    ruleSet[
+                                        `overdue_${modal ? i + 1 : triggerNumber}_restrict`
+                                    ].isFallback,
+                            }"
+                        >
+                            {{
+                                handleRestrictions(
+                                    ruleSet[
+                                        `overdue_${modal ? i + 1 : triggerNumber}_restrict`
+                                    ].value
+                                )
+                            }}
+                        </span>
+                    </td>
+                    <td class="actions" v-if="actions">
+                        <router-link
+                            :to="{
+                                name: 'CirculationTriggersFormEdit',
+                                query: {
+                                    library_id: ruleSet.context.library_id,
+                                    item_type_id: ruleSet.context.item_type_id,
+                                    patron_category_id:
+                                        ruleSet.context.patron_category_id,
+                                    triggerNumber: modal
+                                        ? i + 1
+                                        : triggerNumber,
+                                },
+                            }"
+                            class="btn btn-default btn-xs"
+                            ><i class="fa-solid fa-pencil"></i>
+                            {{ $__("Edit") }}</router-link
+                        >
+                        <router-link
+                            v-if="
                                 ruleSet[
-                                    `overdue_${modal ? i + 1 : triggerNumber}_mtt`
-                                ].isFallback,
-                        }"
-                    >
-                        {{
-                            handleTransport(
-                                ruleSet[
-                                    `overdue_${modal ? i + 1 : triggerNumber}_mtt`
-                                ].value,
-                                "email",
-                                !ruleSet[
-                                    `overdue_${modal ? i + 1 : triggerNumber}_notice`
-                                ].value
-                            )
-                        }}
-                    </span>
-                </td>
-                <!-- Print -->
-                <td
-                    v-if="
-                        ruleSet[
-                            `overdue_${modal ? i + 1 : triggerNumber}_has_rules`
-                        ]
-                    "
-                >
-                    <span
-                        :class="{
-                            fallback:
-                                ruleSet[
-                                    `overdue_${modal ? i + 1 : triggerNumber}_mtt`
-                                ].isFallback,
-                        }"
-                    >
-                        {{
-                            handleTransport(
-                                ruleSet[
-                                    `overdue_${modal ? i + 1 : triggerNumber}_mtt`
-                                ].value,
-                                "print",
-                                !ruleSet[
-                                    `overdue_${modal ? i + 1 : triggerNumber}_notice`
-                                ].value
-                            )
-                        }}
-                    </span>
-                </td>
-                <!-- SMS -->
-                <td
-                    v-if="
-                        ruleSet[
-                            `overdue_${modal ? i + 1 : triggerNumber}_has_rules`
-                        ]
-                    "
-                >
-                    <span
-                        :class="{
-                            fallback:
-                                ruleSet[
-                                    `overdue_${modal ? i + 1 : triggerNumber}_mtt`
-                                ].isFallback,
-                        }"
-                    >
-                        {{
-                            handleTransport(
-                                ruleSet[
-                                    `overdue_${modal ? i + 1 : triggerNumber}_mtt`
-                                ].value,
-                                "sms",
-                                !ruleSet[
-                                    `overdue_${modal ? i + 1 : triggerNumber}_notice`
-                                ].value
-                            )
-                        }}
-                    </span>
-                </td>
-                <!-- Restricts Checkouts -->
-                <td
-                    v-if="
-                        ruleSet[
-                            `overdue_${modal ? i + 1 : triggerNumber}_has_rules`
-                        ]
-                    "
-                >
-                    <span
-                        :class="{
-                            fallback:
-                                ruleSet[
-                                    `overdue_${modal ? i + 1 : triggerNumber}_restrict`
-                                ].isFallback,
-                        }"
-                    >
-                        {{
-                            handleRestrictions(
-                                ruleSet[
-                                    `overdue_${modal ? i + 1 : triggerNumber}_restrict`
-                                ].value
-                            )
-                        }}
-                    </span>
-                </td>
-                <td class="actions" v-if="actions">
-                    <router-link
-                        :to="{
-                            name: 'CirculationTriggersFormEdit',
-                            query: {
-                                library_id: ruleSet.context.library_id,
-                                item_type_id: ruleSet.context.item_type_id,
-                                patron_category_id:
-                                    ruleSet.context.patron_category_id,
-                                triggerNumber: modal ? i + 1 : triggerNumber,
-                            },
-                        }"
-                        class="btn btn-default btn-xs"
-                        ><i class="fa-solid fa-pencil"></i>
-                        {{ $__("Edit") }}</router-link
-                    >
-                    <router-link
-                        v-if="
-                            ruleSet[
-                                `overdue_${modal ? i + 1 : triggerNumber}_has_rules`
-                            ].value && !isOnlyRuleSetForTrigger(triggerNumber)
-                        "
-                        :to="{
-                            name: 'CirculationTriggersFormConfirmReset',
-                            query: {
-                                library_id: ruleSet.context.library_id,
-                                item_type_id: ruleSet.context.item_type_id,
-                                patron_category_id:
-                                    ruleSet.context.patron_category_id,
-                                triggerNumber: triggerNumber,
-                            },
-                        }"
-                        class="btn btn-default btn-xs"
-                        ><i class="fa-solid fa-eraser"></i>
-                        {{ $__("Reset") }}</router-link
-                    >
-                </td>
-            </tr>
+                                    `overdue_${modal ? i + 1 : triggerNumber}_has_rules`
+                                ].value &&
+                                !isOnlyRuleSetForTrigger(triggerNumber)
+                            "
+                            :to="{
+                                name: 'CirculationTriggersFormConfirmReset',
+                                query: {
+                                    library_id: ruleSet.context.library_id,
+                                    item_type_id: ruleSet.context.item_type_id,
+                                    patron_category_id:
+                                        ruleSet.context.patron_category_id,
+                                    triggerNumber: triggerNumber,
+                                },
+                            }"
+                            class="btn btn-default btn-xs"
+                            ><i class="fa-solid fa-eraser"></i>
+                            {{ $__("Reset") }}</router-link
+                        >
+                    </td>
+                </tr>
+            </template>
             <tr v-if="modal">
                 <td colspan="7"></td>
                 <td class="actions">
